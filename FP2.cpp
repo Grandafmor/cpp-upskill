a2ffcc2247d1cdee0dc5fc51660395b5b3351ef8
Grandafmor <piotrex23@gmail.com>
2022-10-07 07:17:03 +0200

    Debugging

--- FP2.cpp
+++ FP2.cpp
@@ -1,26 +1,39 @@
 ﻿#include <iostream>
 #include "doubler.h"
+#define ENABLE_DEBUG
 
 int i{1};
 
 void doPrint()
 {
+#ifdef ENABLE_DEBUG
+   std::cerr<<"doPrint() called\n";
+#endif
    std::cout<<"Dodawanie\n";
 }
 
 void printValue(int x)
 {
+#ifdef ENABLE_DEBUG
+   std::cerr<<"printValue(int x) called\n";
+#endif
    std::cout<<"Liczba "<<i<<" = "<<x<<'\n';
    i++;
 }
 
 int add(int x,int y)
 {
+#ifdef ENABLE_DEBUG
+   std::cerr<<"add(int x,int y) called\n";
+#endif
    return x+y;
 }
 
 int input1()
 {
+#ifdef ENABLE_DEBUG
+   std::cerr<<"input1() called\n";
+#endif
    std::cout<<"Podaj wartosc: ";
    int x{};
    std::cin>>x;
@@ -30,6 +43,9 @@ int input1()
 
 int input2()
 {
+#ifdef ENABLE_DEBUG
+   std::cerr<<"input2() called\n";
+#endif
    std::cout<<"Podaj wartosc: ";
    int y{};
    std::cin>>y;
@@ -39,6 +55,9 @@ int input2()
 
 int main()
 {
+#ifdef ENABLE_DEBUG
+   std::cerr<<"main() called\n";
+#endif
    doPrint();
    int num1{input1()};
    printValue(num1);
@@ -47,6 +66,9 @@ int main()
    std::cout<<num1<<" + "<<num2<<" = "<<add(num1,num2)<<'\n';
    std::cout<<num1<<" * 2 = "<<doubler(num1)<<" \n";
    std::cout<<num2<<" * 2 = "<<doubler(num2)<<" \n";
+#ifdef ENABLE_DEBUG
+   std::cerr<<"main() ended\n";
+#endif
    return 0;
 }
 
