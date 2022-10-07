8e19afb5d8ea72fc88a43158d339e436e6cae567
Grandafmor <piotrex23@gmail.com>
2022-10-07 10:04:13 +0200

    Logging added to doubler function

--- doubler.cpp
+++ doubler.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 #include "doubler.h"
 #include "debug.h"
+#include <plog/Log.h>
 int doubler(int x)
 {
 #ifdef ENABLE_DEBUG
    std::cerr<<"doubler(int x) called\n";
 #endif
+   PLOGD<<"doubler() called";
    return x*2;
 }
